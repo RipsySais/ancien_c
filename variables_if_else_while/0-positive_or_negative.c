d4b203235c8eb84458313a4ee845231055ea590b
RipsySais <cysseahmed@gmail.com>
2024-06-19 11:31:14 +0200

    Script 0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - nbr positif ou negatif
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d \n", n);
+	if (n > 0)
+	{
+		printf("le numero est positive \n");
+	}
+	else if (n < 0)
+
+	{
+		printf("le numero est negetive \n");
+	}
+	else
+	{
+		printf("le numero est de zero \n");
+		return (0);
+	}
