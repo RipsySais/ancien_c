44549ef2aa34ee55418f0485d609ccc87fafc842
RipsySais <cysseahmed@gmail.com>
2024-06-21 11:51:24 +0200

    Script 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - imprimer l'alphabet en miniscule sauf E et Q
+ * Return: (success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'e' && alpha != 'q')
+		putchar(alpha);
+	}
+	putchar(alpha);
+	return (0);
+}
