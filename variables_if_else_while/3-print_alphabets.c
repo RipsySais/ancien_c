548d2179e44c8d32299e8b5eb801f7b65d379982
RipsySais <cysseahmed@gmail.com>
2024-06-21 10:42:09 +0200

    Script 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - return l'alphabet en minuscule et en majuscule
+ * Return: (success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
