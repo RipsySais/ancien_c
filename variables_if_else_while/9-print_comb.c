b62932e48bc5993936c4221d98b2eefd6ac9cdc1
RipsySais <cysseahmed@gmail.com>
2024-06-23 12:56:28 +0200

    Script 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - imprimer toutes les combinaison possible du nombre a un chiffre
+ * Return: 0
+ */
+int main(void)
+{
+	int nombre;
+
+	{
+		for (nombre = 0; nombre <= 9; nombre++)
+			putchar((nombre % 10) + '0');
+		if (nombre == 9)
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
