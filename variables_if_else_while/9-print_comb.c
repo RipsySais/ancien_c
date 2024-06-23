5c6cfe8ea07f2dce8fad9b125913ec147b8a2c2b
RipsySais <cysseahmed@gmail.com>
2024-06-23 23:07:04 +0200

    Script 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,9 +8,9 @@ int main(void)
 	int nombre;
 
 	{
-		for (nombre = 0; nombre <= 9; nombre++)
+		for (nombre = 0; nombre <= 8; nombre++)
 			putchar((nombre % 10) + '0');
-		if ((nombre == 9)continu)
+		if (nombre == 8)
 
 		putchar(',');
 		putchar(' ');
