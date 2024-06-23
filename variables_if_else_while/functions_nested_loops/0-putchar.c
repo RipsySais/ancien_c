a288bde3c1fe4571bda5f485b7aa28e85ece8503
RipsySais <cysseahmed@gmail.com>
2024-06-24 00:19:12 +0200

    Script 0

---
+++ variables_if_else_while/functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ * main - imprime _putchar, suivi d'une nouvelle ligne.
+ * Return: 0
+ */
+int main(0)
+{
+	{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	}
+	putchar('\n');
+	return (0);
+}
