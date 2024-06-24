6f3116052ba6f55a704c387d142e17cfaa604d81
RipsySais <cysseahmed@gmail.com>
2024-06-24 16:41:21 +0200

    Script 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - imprime les nombres, de 0 à 9, suivis d'une nouvelle ligne sans 2 et 4
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int numero;
+
+	for (numero = 48; numero < 58; numero++)
+	{
+		if ((numero == 50) || (numero == 52))
+	}
+	_putchar(numero);
+	_putchar(10);
+}
