e905f5f51445f9d169de2ccad00e4dbfbb9bff5b
RipsySais <cysseahmed@gmail.com>
2024-06-24 14:09:46 +0200

    Script 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isdigit - function qui vérifie si un carectère est un chiffre ou non
+ * @c : caractère doit etre testé
+ * Return: retoune 1 s'il est en majuscule sinon 0
+ */
+
+int int _isdigit(int c);
+{
+	if((c >= 48) && (c <= 57))
+		return (1);
+
+	else
+		return (0);
+}
