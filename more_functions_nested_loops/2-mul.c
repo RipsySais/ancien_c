bff914df08896d0648e7d50dd3f1c76ae522afcf
RipsySais <cysseahmed@gmail.com>
2024-06-24 14:44:56 +0200

    Script 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - une fonction qui multiplie deux entiers
+ * @a : prémier number
+ * @b : deuxième number
+ * Return: retoune les entiers
+ */
+int int mul(int a, int b)
+
+{
+	return (a * b);
+}
