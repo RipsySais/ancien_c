1cf7e992da57ba3735084cc217bc874e4647b20c
RipsySais <cysseahmed@gmail.com>
2024-06-24 14:20:52 +0200

    Script 1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -5,12 +5,11 @@
  * @c : caractère doit etre testé
  * Return: retoune 1 s'il est en majuscule sinon 0
  */
+int _isdigit(int c)
 
-int int _isdigit(int c);
 {
-	if((c >= 48) && (c <= 57))
+	if ((c >= 48) && (c <= 57))
 		return (1);
-
 	else
 		return (0);
 }
