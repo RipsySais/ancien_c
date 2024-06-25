527b72910f6eaafaade211285ae9e82ba45b3409
RipsySais <cysseahmed@gmail.com>
2024-06-25 16:50:59 +0200

    Script 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
@@ -29,7 +29,7 @@ int main(void)
 		{
 			printf(" ");
 		}
-		printf("\n");
 	}
+	printf("\n");
 	return (0);
 }
