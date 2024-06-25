cbf1a84f04bde857bfb826d6bbcdb6fde6283bae
RipsySais <cysseahmed@gmail.com>
2024-06-25 14:57:39 +0200

    Script 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,7 +6,6 @@
  * Return: Always
  */
 void print_diagonal(int n)
-
 {
 	int i, j;
 
@@ -16,7 +15,7 @@ void print_diagonal(int n)
 	}
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
