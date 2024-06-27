ca4e39ab21670990d1d306defb976bd0cb5705a6
RipsySais <cysseahmed@gmail.com>
2024-06-27 16:16:31 +0200

    Script 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - une fonction qui imprime une chaine
+ * @str: a function that prints a string
+ */
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\0');
+
+	_putchar('\n');
+}
