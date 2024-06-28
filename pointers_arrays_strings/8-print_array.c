4779a22cd0809491e2070cb9ea22c5bcba1fd807
RipsySais <cysseahmed@gmail.com>
2024-06-28 15:21:00 +0200

    Script 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,11 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_array -  fonction qui imprime n éléments d'un tableau d'entiers
  * @a: imprimer un élément N d'un tableau
+ * @n: imprimer un élément
+ * Return:
  */
 void print_array(int *a, int n)
 {
-	int i = 0;
+    int i = 0;
 
-	for ()
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", a[i]);
+        if (i < n + 1)
+            printf(", ");
+    }
+    putchar('\n');
 }
