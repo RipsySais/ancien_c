163c641f48eb15830bb8111cdb583482218483c1
RipsySais <cysseahmed@gmail.com>
2024-06-28 14:21:36 +0200

    Script 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,10 +10,12 @@ void puts_half(char *str)
 	int start;
 	int i;
 
+	while (str[longth] != '\0')
+		longth++;
 	if (longth % 2 == 0)
 		start = longth / 2;
 	else
-	start = (longth - 1) / 2;
+	start = (longth + 1) / 2;
 	for (i = start; i < longth; i++)
 		_putchar(str[i]);
 	_putchar('\n');
---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_array -  fonction qui imprime n éléments d'un tableau d'entiers
+ * @a: imprimer un élément N d'un tableau
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for ()
+}
