7c819b646cefe9bac157b03ca5b68216291797fb
RipsySais <cysseahmed@gmail.com>
2024-06-28 12:33:13 +0200

    Script 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - fonction qui imprime la moitié d'une chaîne
+ * @str: a function that prints
+ */
+void puts_half(char *str)
+{
+	int longth = 0;
+	int start;
+	int i;
+
+	if (longth % 2 == 0)
+		start = longth / 2;
+	else
+	start = (longth - 1) / 2;
+	for (i = start; i < longth; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
