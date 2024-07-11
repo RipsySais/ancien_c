20be6ea649e52c3eddb41934ca22aef41fe2eab6
RipsySais <cysseahmed@gmail.com>
2024-07-11 15:14:51 +0200

    Script 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,11 +1,15 @@
+#include <stdlib.h>
 #include <stdio.h>
 /**
- * un programme qui imprime son nom
+ * main - un programme qui imprime son nom
+ * @argc: variable
+ * @argv: pointeur
  * Return: 0
  */
-
-int main()
+int main(int argc, char **argv)
 {
-	printf ("le nom du programme: %s\n", _FILE_);
+	(void)argc;
+
+	printf("%s\n", argv[0]);
 	return (0);
 }
