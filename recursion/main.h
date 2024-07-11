322afae344f3facf688d120fde17ad388363ee24
RipsySais <cysseahmed@gmail.com>
2024-07-11 11:56:24 +0200

    Script 0

--- recursion/main.h
+++ recursion/main.h
@@ -1,2 +1,10 @@
- void _puts_recursion(char *s);
- void _print_rev_recursion(char *s);
+#ifndef MAIN_H
+#define MAIN_H
+
+int	_putchar(char c);
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+char *create_array(unsigned int size, char c);
+
+#endif
