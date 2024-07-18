df0ad4da9c9a8121aa8a4136d77b37e62e729582
RipsySais <cysseahmed@gmail.com>
2024-07-18 11:45:11 +0200

    Script 4

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -8,15 +8,16 @@
  */
 void free_grid(int **grid, int height)
 {
-	int i;
+	int i = 0;
 
-	if (grid == NULL || height < 0)
+	if (grid == NULL)
 	{
 		return;
 	}
-	for (i = 0; i < height; i++)
+	while (i < height)
 	{
 		free(grid[i]);
+		i++;
 	}
 	free(grid);
 }
