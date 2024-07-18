6f86a8dbf67f8c1cfbca769a6270d1fc449f93b6
RipsySais <cysseahmed@gmail.com>
2024-07-18 14:04:11 +0200

    Script 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - une fonction qui alloue
+ * de la mémoire à l'aide de malloc
+ * @b:  une fonction qui alloue
+ * Return: ptr
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
